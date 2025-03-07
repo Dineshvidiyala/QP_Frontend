d1b32f84d36dae8daf143dd47b4846b52840afdd
Dineshvidiyala <154747381+Dineshvidiyala@users.noreply.github.com>
2025-03-07 10:47:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -200,6 +200,10 @@ function displayQuestionPaper(questions, paperDetails, allowEdit = true) {
                 `).join('')}
                 </tbody>
             </table>
+            <br>
+            <br>
+            <br>
+            <p style="text-align: center;"><strong>*ALL THE BEST*</strong></p>
         </div>
     `;
     document.getElementById('questionPaper').innerHTML = html;
@@ -367,6 +371,10 @@ async function downloadQuestionPaper() {
                 `).join('')}
                 </tbody>
             </table>
+            <br>
+            <br>
+            <br>
+            <p style="text-align: center;"><strong>*ALL THE BEST*</strong></p>
         </div>
     `;
     hiddenContainer.innerHTML = html;
